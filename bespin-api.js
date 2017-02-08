a95a27cf27d591fb39c295c3d5f83680104d554a
John Bradley <johnbradley2008@gmail.com>
2017-02-08 11:52:43 -0500

    bespin api tests

--- bespin-api.js
+++ bespin-api.js
@@ -1,28 +1,28 @@
 var request = require('request');
 
-
 function BespinApi(config) {
     return {
         verifyToken: function (jobId, token, onValidToken, onInvalidToken) {
-            console.log(jobId, token, onValidToken, onInvalidToken);
             var options = this.makeRequestOptions(jobId, token);
             request(options, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     onValidToken(jobId);
                 } else {
                     var errorMessage = 'Checking authorization failed with status:' + response.statusCode + ":" + error;
-
                     onInvalidToken(errorMessage);
                 }
             });
         },
         makeURL: function(jobId) {
-            return 'http://'+ config.bespinapihost + ':' + config.bespinapiport + '/api/jobs/' + jobId + '/';
+            var protocol = config.bespinapiprotocol;
+            var host = config.bespinapihost;
+            var port = config.bespinapiport;
+            return protocol + '://'+ host + ':' + port + '/api/jobs/' + jobId + '/';
         },
         makeRequestOptions: function(jobId, token) {
             return {
                 url: this.makeURL(jobId),
-                    headers: {
+                headers: {
                         'Authorization': 'Token ' + token
                 }
             };
---
+++ tests/bespoin-api-tests.js
@@ -0,0 +1,77 @@
+var chai = require('chai');
+var expect = chai.expect;
+var proxyquire =  require('proxyquire')
+    , assert     =  require('assert')
+    , requestStub   =  { };
+
+var lastRequest = {};
+function fakeRequest(options, func) {
+    lastRequest.options = options;
+    lastRequest.func = func;
+}
+var BespinApi = proxyquire('../bespin-api', { 'request': fakeRequest });
+
+describe('BespinApi', function() {
+    it('makeRequestOptions() builds structure with expected URL', function() {
+        var config = {
+            bespinapiprotocol: "https",
+            bespinapihost: "127.0.0.1",
+            bespinapiport: "12345"
+        };
+        var jobId = "541";
+        var token = "abcdefg";
+        var bespinApi = BespinApi(config);
+        var requestOptions = bespinApi.makeRequestOptions(jobId, token);
+        expect(requestOptions.url).to.equal('https://127.0.0.1:12345/api/jobs/541/');
+        expect(requestOptions.headers['Authorization']).to.equal('Token abcdefg');
+    });
+    it('verifyToken() calls onValidToken when good', function() {
+        var config = {
+            bespinapiprotocol: "https",
+            bespinapihost: "127.0.0.1",
+            bespinapiport: "12345"
+        };
+        var jobId = "541";
+        var token = "abcdefg";
+        var bespinApi = BespinApi(config);
+        var result = '';
+        function onValidToken(jobId) {
+            result = 'Valid ' + jobId;
+        }
+        function onInvalidToken(error) {
+            result = 'Error ' + error;
+        }
+        bespinApi.verifyToken(jobId, token, onValidToken, onInvalidToken);
+        var error = '';
+        var response = {
+            statusCode: 200
+        };
+        lastRequest.func(error, response);
+        expect(result).to.equal('Valid 541');
+    });
+    it('verifyToken() calls onInvalidToken when bad', function() {
+        var config = {
+            bespinapiprotocol: "https",
+            bespinapihost: "127.0.0.1",
+            bespinapiport: "12345"
+        };
+        var jobId = "541";
+        var token = "abcdefg";
+        var bespinApi = BespinApi(config);
+        var result = '';
+        function onValidToken(jobId) {
+            result = 'Valid ' + jobId;
+        }
+        function onInvalidToken(error) {
+            result = 'Error ' + error;
+        }
+        bespinApi.verifyToken(jobId, token, onValidToken, onInvalidToken);
+        var error = 'bad';
+        var response = {
+            statusCode: 404,
+            error: 'oops'
+        };
+        lastRequest.func(error, response);
+        expect(result).to.equal('Error Checking authorization failed with status:404:bad');
+    });
+});
