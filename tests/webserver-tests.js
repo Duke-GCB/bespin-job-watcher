13284c172f073494c05f5f8fa80e2fc1b8d58cb2
John Bradley <johnbradley2008@gmail.com>
2017-02-09 12:51:37 -0500

    Webserver broadCastJobStatus test

--- tests/webserver-tests.js
+++ tests/webserver-tests.js
@@ -14,11 +14,22 @@ function fakeExpress() {
     }
 }
 
+var jobWatcherData = {};
+function fakeJobWatchers() {
+    return {
+        notify: function(jobId, data) {
+            jobWatcherData.jobId = jobId;
+            jobWatcherData.data = data;
+        }
+    }
+}
+
 var Webserver = proxyquire('../webserver', {
     'fs': fsStub,
     'express': fakeExpress,
     'https': httpsStub,
-    'ws': wsStub
+    'ws': wsStub,
+    './job-watchers': fakeJobWatchers
 });
 
 describe('Webserver', function() {
@@ -63,5 +74,42 @@ describe('Webserver', function() {
         expect(createServerOptions.cert).to.equal('certdata');
         expect(serverListenOptions.host).to.equal('121.0.0.1');
         expect(serverListenOptions.port).to.equal(8081);
+    });
+    it('broadCastJobStatus will call jobWatchers.notify', function() {
+        var config = {
+            webserver: {
+                "host": "121.0.0.1",
+                "port": 8081,
+                "key": "mykey.pem",
+                "cert": "mycert.pem"
+            }
+        };
+        var createServerOptions = {};
+        var serverListenOptions = {};
+        httpsStub.createServer = function (options, app) {
+            createServerOptions = options;
+            return {}
+        };
+        wsStub.Server = function (server) {
+            return {
+                on: function () {
+
+                }
+            };
+        };
+        fsStub.readFileSync = function (filename) {
+            if (filename === 'mykey.pem') {
+                return 'keydata';
+            }
+            if (filename === 'mycert.pem') {
+                return 'certdata';
+            }
+            return '';
+        };
+        var webserver = Webserver(config);
+        jobWatcherData = {};
+        webserver.broadCastJobStatus('{"job":1, "status":"Not good"}');
+        expect(jobWatcherData.jobId).to.equal(1);
+        expect(jobWatcherData.data).to.deep.equal({"job":1, "status":"Not good"});
     });
 });
